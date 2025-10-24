45044ac073a777192a6254fee9c78392bb81eabd
shayan <shayan@vloom.ai>
2025-10-24 10:56:29 -0700

    small tweak to make sure everything works

--- include/mainPageUI.hpp
+++ include/mainPageUI.hpp
@@ -1,4 +1,4 @@
 #pragma once
-#include <SFMLGraphics.hpp>
+#include <SFML/Graphics.hpp>
 
 void backGroundUi();
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,6 @@
 
 int main() {
     
-    void backGroundUi();
+    backGroundUi();
     return 0;
 }
